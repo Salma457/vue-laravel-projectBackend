60dd0208d47ed76bcd64a73ebb1bd7428edac7ea
salma457 <you@example.com>
2025-05-19 18:05:48 +0300

    update migration files and add fillable to candidate

--- app/Models/Candidate.php
+++ app/Models/Candidate.php
@@ -6,6 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 use App\Models\Application;
 class Candidate extends Model
 {
+    protected $fillable = [
+    'user_id',
+    'current_job',
+    'experience_level',
+    'highest_qualification',
+    'bio',
+    'resume',
+    'linkedin_profile',
+    'phone_number',
+    'location'
+];
     public function applications()
 {
     return $this->hasMany(Application::class);
